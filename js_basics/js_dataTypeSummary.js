867818e9ed0b1f8aa91ab4ccb24bc963cb9089b0
paljyoti04 <134566457+paljyoti04@users.noreply.github.com>
2026-02-28 11:39:00 +0000

    dataTypeSummary.js

---
+++ js_basics/js_dataTypeSummary.js
@@ -0,0 +1,33 @@
+//primitive Data type 
+
+// 7 types: Number, Boolean, String, Symbol, BigInt, Null, Undefined
+
+const score = 55
+const isLoggedIn = false
+const name = "jyoti"
+const bigNumber = 23456778478374987n
+const outsideTem = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId);
+
+
+// Non Primitive or Refernce type
+
+// Array, object, Functions
+
+const hros = ["shaktiman", "nagraj", "doga"]
+
+let myObj = {
+    name: "jyoti",
+    age: 19,
+
+}
+
+let myFunction = function(){
+    console.log("hello world");
+}
+
