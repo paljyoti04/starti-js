b50c6c366736c8ff52c3526f3da9959061a8836b
paljyoti04 <134566457+paljyoti04@users.noreply.github.com>
2026-02-28 10:38:14 +0000

    js_datatype.js

---
+++ js_basics/js_datatype.js
@@ -0,0 +1,9 @@
+"use strict" //treat all js code as newer version 
+
+//alert( 3+3 )  //it's correct for browser but we are use nodejs, not browser 
+let age = 18;
+let name = "jyoti";
+
+console.log(typeof age);
+console.table([typeof name, typeof age]);
+
